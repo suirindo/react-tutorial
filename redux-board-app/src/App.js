d11119472f59a1db5a09ce442c970a559ee9901b
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-24 19:14:37 +0900

    0924

---
+++ redux-board-app/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const postList = useSelector((state) => state.posts.value);
+  console.log(postList);
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input type="text" placeholder="お名前" />
+        <input type="text" placeholder="投稿内容" />
+        <button>投稿</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postList.map((post) => (
+          <div key={post.id} className="post">
+            <h1>{post.name} </h1>
+            <h1 className="postContent">{post.content} </h1>
+            <button>削除</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-board-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import postsReducer from './features/Posts';
+
+const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* AppをProviderでラップして、storeをどこでも利用可能な状態にしている */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
