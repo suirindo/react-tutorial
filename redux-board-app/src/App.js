73514e61dc32ca24255aabb5df5fc32dffd7ed74
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-24 19:37:16 +0900

    0924

--- redux-board-app/src/App.js
+++ redux-board-app/src/App.js
@@ -1,26 +1,57 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { addPost, deletePost } from './features/Posts';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
   const postList = useSelector((state) => state.posts.value);
   console.log(postList);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addPost({
+        id: postList.length,
+        name: name,
+        content: content,
+      })
+    );
+
+    setName('');
+    setContent('');
+  };
   return (
     <div className="App">
       <div>
         <h1>React-Redux掲示板</h1>
       </div>
       <div className="addPost">
-        <input type="text" placeholder="お名前" />
-        <input type="text" placeholder="投稿内容" />
-        <button>投稿</button>
+        <input
+          type="text"
+          placeholder="お名前"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="投稿内容"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>投稿</button>
         <hr />
       </div>
       <div className="displayPosts">
         {postList.map((post) => (
           <div key={post.id} className="post">
-            <h1>{post.name} </h1>
+            <h1 className="postName">{post.name} </h1>
             <h1 className="postContent">{post.content} </h1>
-            <button>削除</button>
+            <button onClick={() => dispatch(deletePost({ id: post.id }))}>
+              削除
+            </button>
           </div>
         ))}
       </div>
--- redux-board-app/src/features/Posts.js
+++ redux-board-app/src/features/Posts.js
@@ -8,6 +8,10 @@ export const postSlice = createSlice({
     addPost: (state, action) => {
       state.value.push(action.payload);
     },
+    deletePost: (state, action) => {
+      state.value = state.value.filter((post) => post.id !== action.payload.id);
+    },
   },
 });
 export default postSlice.reducer;
+export const { addPost, deletePost } = postSlice.actions;
